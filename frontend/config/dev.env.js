d9514fc7060ae33e1e34f57e765222f44be216e3
agile6v <agile6v@agile6v.com>
2018-03-16 22:08:32 +0800

    Initial frontend

---
+++ frontend/config/dev.env.js
@@ -0,0 +1,8 @@
+'use strict'
+const merge = require('webpack-merge')
+const prodEnv = require('./prod.env')
+
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"development"',
+  BASE_API: '"https://easy-mock.com/mock/5aa4af3fd2fb81037174730a"',
+})
---
+++ frontend/config/prod.env.js
@@ -0,0 +1,5 @@
+'use strict'
+module.exports = {
+  NODE_ENV: '"production"',
+  BASE_API: '"https://easy-mock.com/mock/5aa4af3fd2fb81037174730a"',
+}
