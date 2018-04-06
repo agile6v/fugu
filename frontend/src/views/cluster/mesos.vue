3040cfc07bb2eb37179ffeda0a93064d650e9d32
agile6v <agile6v@agile6v.com>
2018-04-06 10:01:18 +0800

    Add mesos cluster management

--- frontend/src/views/cluster/mesos.vue
+++ frontend/src/views/cluster/mesos.vue
@@ -1,14 +1,82 @@
 <template>
-  <div class="temp-container">
-    <p class="warn-content">
-      即将来临
-    </p>
+  <div class="cluster-container">
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <strong style="display:inline-block;">Clusters</strong>
+        <el-button class="filter-item" style="display:inline-block;" type="primary" plain icon="el-icon-check">Add</el-button>
+      </div>
+      <el-table :data="tableData5" stripe style="width: 100%">
+        <el-table-column type="expand">
+          <template slot-scope="props">
+            <el-form label-position="left" inline class="demo-table-expand">
+              <el-form-item label="商品名称">
+                <span>{{ props.row.name }}</span>
+              </el-form-item>
+              <el-form-item label="所属店铺">
+                <span>{{ props.row.shop }}</span>
+              </el-form-item>
+              <el-form-item label="商品 ID">
+                <span>{{ props.row.id }}</span>
+              </el-form-item>
+              <el-form-item label="店铺 ID">
+                <span>{{ props.row.shopId }}</span>
+              </el-form-item>
+              <el-form-item label="商品分类">
+                <span>{{ props.row.category }}</span>
+              </el-form-item>
+              <el-form-item label="店铺地址">
+                <span>{{ props.row.address }}</span>
+              </el-form-item>
+              <el-form-item label="商品描述">
+                <span>{{ props.row.desc }}</span>
+              </el-form-item>
+            </el-form>
+          </template>
+        </el-table-column>
+        <el-table-column label="Name" prop="name">
+        </el-table-column>
+        <el-table-column label="Description" prop="desc">
+        </el-table-column>
+        <el-table-column label="Operations">
+          <template slot-scope="scope">
+          <el-button
+            size="mini"
+            @click="handleEdit(scope.$index, scope.row)">Edit</el-button>
+          <el-button
+            size="mini"
+            type="danger"
+            @click="handleDelete(scope.$index, scope.row)">Delete</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'i18n',
+  name: 'mesos',
+  data () {
+    return {
+      tableData5: [{
+        id: '12987122',
+        name: 'agile6v-cluster-1',
+        category: '江浙小吃、小吃零食',
+        desc: '荷兰优质淡奶，奶香浓而不腻',
+        address: '上海市普陀区真北路',
+        shop: '王小虎夫妻店',
+        shopId: '10333'
+      }, {
+        id: '12987123',
+        name: 'agile6v-cluster-2',
+        category: '江浙小吃、小吃零食',
+        desc: '荷兰优质淡奶，奶香浓而不腻',
+        address: '上海市普陀区真北路',
+        shop: '王小虎夫妻店',
+        shopId: '10333'
+      }]
+    }
+  },
   computed: {
     lang: {
       get() {
@@ -24,13 +92,11 @@ export default {
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
-.temp-container {
-  margin: 10px 20px 0;
-  overflow: hidden;
-}
-.warn-content {
-  text-align: center;
-  color: #42b983;
-  font-weight: 600;
+.cluster-container {
+  padding: 20px;
+  background-color: rgb(240, 242, 245);
+  .box-card {
+    margin-top: 10px;
+  }
 }
 </style>
